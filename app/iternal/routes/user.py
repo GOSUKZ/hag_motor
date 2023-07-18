6908a6fd8c9623e5b538ce39eaea54ce6c4c6b98
artem.drozdov <a.drozdov@gosu.kz>
2023-07-18 16:30:57 +0600

    Update user.py

--- app/iternal/routes/user.py
+++ app/iternal/routes/user.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Body
+from fastapi import APIRouter, Body, Depends
 from ..db.user import addUser, getAllUsers, updateUser, delUser
 from ..db.db import client
 from ..models.user import User, UpdateUser
@@ -6,44 +6,36 @@ from ..serializers.user import userEntity
 
 from fastapi.encoders import jsonable_encoder
 
+router = APIRouter(prefix="/user", tags=["Users"])
 
-router = APIRouter(
-    prefix="/user",
-    tags=["Users"]
-)
+
+async def get_mongodb_session():
+    async with await client.start_session() as session:
+        yield session
+
 
 @router.post("/", response_description="Success")
-async def root(user : User = Body(...)):
+async def root(user: User = Body(...), session=Depends(get_mongodb_session)):
     user = jsonable_encoder(user)
-    # Use "await" for start_session, but not for start_transaction.
-    async with await client.start_session() as s:
-        user = await addUser(s, user)
-        user = userEntity(user)
-    return user
+    user = await addUser(session, user)
+    return userEntity(user)
+
 
 @router.put("/", response_description="Success")
-async def root(user : UpdateUser = Body(...)):
+async def root(user: UpdateUser = Body(...), session=Depends(get_mongodb_session)):
     user = jsonable_encoder(user)
-    # Use "await" for start_session, but not for start_transaction.
-    async with await client.start_session() as s:
-        user = await updateUser(s, user)
-        user = userEntity(user)
-    return user
+    user = await updateUser(session, user)
+    return userEntity(user)
+
 
 @router.delete("/", response_description="Success")
-async def root(id: str):
-    # Use "await" for start_session, but not for start_transaction.
-    async with await client.start_session() as s:
-        user = await delUser(s, id)
-        user = userEntity(user)
-    return user
+async def root(id: str, session=Depends(get_mongodb_session)):
+    user = await delUser(session, id)
+    return userEntity(user)
+
 
 @router.get("/", response_description="Success")
-async def root():
-    # Use "await" for start_session, but not for start_transaction.
-    async with await client.start_session() as s:
-        users_db = await getAllUsers(s)
-        users = []
-        for user in users_db:
-            users.append(userEntity(user))
+async def root(session=Depends(get_mongodb_session)):
+    users_db = await getAllUsers(session)
+    users = [userEntity(user) for user in users_db]
     return users
