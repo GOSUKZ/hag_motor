a41ccf3c849e65b8733c6068c2475f97f2238cbb
artem.drozdov <a.drozdov@gosu.kz>
2023-07-18 15:26:17 +0600

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI
-from app.configuration.server import Server
+# from fastapi import FastAPI
+# from app.configuration.server import Server
 
-def create_app(_=None) -> FastAPI:
-    app = FastAPI()
+# def create_app(_=None) -> FastAPI:
+#     app = FastAPI()
     
-    return Server(app).get_app()
+#     return Server(app).get_app()
