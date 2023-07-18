67a10b4280de4dfd960ac2dee7396081a66ba938
artem.drozdov <a.drozdov@gosu.kz>
2023-07-18 18:53:31 +0600

    test fix

--- app/configuration/server.py
+++ app/configuration/server.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
 from app.configuration.routes import __routes__
-from app.iternal.events import events
+# from app.iternal.events import events
+from app.iternal.config import settings
+from motor.motor_asyncio import AsyncIOMotorClient
+DATABASE_URL = settings.DATABASE_URL
+
+def startup_event(app) -> AsyncIOMotorClient:
+    app.state.mongodb = AsyncIOMotorClient(DATABASE_URL)
+    print("startup_event")
+    
+def shutdown_event(app):
+    app.state.mongodb.close()
+    print("shutdown_event")
 
 class Server:
     __app: FastAPI
@@ -16,8 +27,8 @@ class Server:
 
     @staticmethod
     def __register_events(app):
-        app.on_event("startup")(lambda: events.startup_event(app))
-        app.on_event("shutdown")(lambda: events.shutdown_event(app))
+        app.on_event("startup")(lambda: startup_event(app))
+        app.on_event("shutdown")(lambda: shutdown_event(app))
 
     @staticmethod
     def __register_routs(app):
