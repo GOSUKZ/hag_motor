c30de5b7f64d2926856956d111f3cdf3fd8ea5a6
artem.drozdov <a.drozdov@gosu.kz>
2023-07-19 10:20:41 +0600

    optimization

--- app/configuration/server.py
+++ app/configuration/server.py
@@ -1,21 +1,9 @@
 from fastapi import FastAPI
 from app.configuration.routes import __routes__
-# from app.iternal.events import events
+from app.iternal.events import events
 from app.iternal.config import settings
-from motor.motor_asyncio import AsyncIOMotorClient
-import asyncio
 
 DATABASE_URL = settings.DATABASE_URL
-
-def startup_event(app) -> AsyncIOMotorClient:
-    client = AsyncIOMotorClient(DATABASE_URL)
-    client.get_io_loop = asyncio.get_event_loop
-    app.state.mongodb = client
-    print("startup_event")
-    
-def shutdown_event(app):
-    app.state.mongodb.close()
-    print("shutdown_event")
 
 class Server:
     __app: FastAPI
@@ -24,7 +12,7 @@ class Server:
         self.__app = app
         self.__register_routs(app)
         self.__register_events(app)
-        startup_event(app)
+        # startup_event(app) # *if the startup_event does not fire, uncomment it
 
 
     def get_app(self) -> FastAPI:
@@ -32,8 +20,8 @@ class Server:
 
     @staticmethod
     def __register_events(app):
-        # app.on_event("startup")(lambda: startup_event(app))
-        app.on_event("shutdown")(lambda: shutdown_event(app))
+        app.on_event("startup")(lambda: events.startup_event(app))
+        app.on_event("shutdown")(lambda: events.shutdown_event(app))
 
     @staticmethod
     def __register_routs(app):
--- app/iternal/events/events.py
+++ app/iternal/events/events.py
@@ -1,15 +1,18 @@
+import asyncio
+from app.iternal.config import settings
+from motor.motor_asyncio import AsyncIOMotorClient
 from app.iternal.config import settings
 
 DATABASE_URL = settings.DATABASE_URL
-
-from motor.motor_asyncio import AsyncIOMotorClient
-
+MONGO_INITDB_DATABASE = settings.MONGO_INITDB_DATABASE
 
 def startup_event(app) -> AsyncIOMotorClient:
-    app.state.mongodb = AsyncIOMotorClient(DATABASE_URL)
+    client = AsyncIOMotorClient(DATABASE_URL)
+    client.get_io_loop = asyncio.get_event_loop
+    app.state.mongodb = client
+    app.state.database = app.state.mongodb[MONGO_INITDB_DATABASE]
     print("startup_event")
     
 def shutdown_event(app):
     app.state.mongodb.close()
     print("shutdown_event")
-    
