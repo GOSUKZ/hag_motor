c8c47121bb6685ce75c0b85b5ed2b338894ff205
artem.drozdov <a.drozdov@gosu.kz>
2023-07-25 10:50:37 +0600

    test

--- app/iternal/routes/file.py
+++ app/iternal/routes/file.py
@@ -14,7 +14,7 @@ router = APIRouter(
 async def upload_file(request: Request, file: UploadFile = File(...)):
     try:
         # Read the Excel file using Pandas
-        df = pd.read_excel(file.file, engine="openpyxl")  # Assuming the file is in .xlsx format.
+        # df = pd.read_excel(file.file, engine="openpyxl")  # Assuming the file is in .xlsx format.
 
         # df_filled_with_zero = df.fillna(0) # Fill the empty with zero values
         # array_data = df_filled_with_zero.to_numpy().tolist() # Convert to array
