b52999fbbbfa7d386fe8d4eae324086d4372b3f5
artem.drozdov <a.drozdov@gosu.kz>
2023-07-27 10:43:50 +0600

    add ReExported_data

--- app/iternal/routes/file.py
+++ app/iternal/routes/file.py
@@ -1,3 +1,4 @@
+import pathlib
 from fastapi.responses import StreamingResponse
 import io
 from fastapi import APIRouter, Body, File, UploadFile, Request
@@ -16,44 +17,120 @@ router = APIRouter(
 
 @router.post("/upload/")
 async def upload_file(request: Request, file: UploadFile = File(...)):
+    # Read the Excel file using Pandas from the file's content
+    content = await file.read()
+
     try:
-        # Read the Excel file using Pandas from the file's content
-        content = await file.read()
+        # Assuming the file is in .xlsx format.
+        control_data_df = pd.read_excel(
+            BytesIO(content), engine="openpyxl", sheet_name="__control_data")
+
+        # Fill the empty with zero values ( df.fillna(0) )
+        control_data_df_filled_with_zero = control_data_df.where(
+            pd.notnull(control_data_df), None)
+
+        # Convert to array
+        control_data_array_data = control_data_df_filled_with_zero.to_numpy().tolist()
+
+    except Exception as e:
+        print({"message": str(e)})
+
+    control_data = {}
+
+    try:
+        # Check if the array
+        if control_data_array_data:
+
+            # limit the reading area
+            control_data_array_keys = control_data_array_data[2:3]
+            control_data_array_data = control_data_array_data[3:]
+
+            # Collect data from control_data sheet
+            for i in range(0, len(control_data_array_keys)):
+                key = control_data_array_keys[i][0]
+                data = control_data_array_data[i][0]
+                control_data[key] = data
+
+    except Exception as e:
+
+        control_data = {}
+
+        print({"message": str(e)})
+
+    try:
         # Assuming the file is in .xlsx format.
         df = pd.read_excel(BytesIO(content), engine="openpyxl")
 
-        df_filled_with_zero = df.fillna(0)  # Fill the empty with zero values
+        # Fill the empty with zero values ( df.fillna(0) )
+        df_filled_with_zero = df.where(pd.notnull(df), None)
         array_data = df_filled_with_zero.to_numpy().tolist()  # Convert to array
 
-        # limit the reading area
-        # * -1 because we don't read the total and 4 because we don't read header
-        array_data = array_data[3:-1]
-        for i in range(len(array_data)):
-            array_data[i] = array_data[i][:19]
+        if not control_data:
+            # limit the reading area
+            # * -1 because we don't read the total and 4 because we don't read header
+            array_data = array_data[3:-1]
+            for i in range(len(array_data)):
+                array_data[i] = array_data[i][:19]
 
-        title_array = ['Дата', 'Место отправки', 'Вид отправки', 'Телефон клиента', 'Код клиента', 'Описание груза ', 'Количество мест', 'Вес', 'Плотность места', 'Плотность',
-                       'Место доставки', 'За упаковку', 'За доставку', 'Разное', 'Страховка', 'Цена за единицу', 'Общая сумма', 'Дата прихода', 'Количество полученных позицый']
-        data_array = array_data[1:]
+            title_array = ['Дата', 'Место отправки', 'Вид отправки', 'Телефон клиента', 'Код клиента', 'Описание груза ', 'Количество мест', 'Вес', 'Плотность места', 'Плотность',
+                           'Место доставки', 'За упаковку', 'За доставку', 'Разное', 'Страховка', 'Цена за единицу', 'Общая сумма', 'Дата прихода', 'Количество полученных позицый']
+            data_array = array_data[1:]
 
-        data_for_db = []
-        for i in range(len(data_array)):
-            line_for_db = {}
-            for j in range(len(title_array)):
-                title = str(title_array[j])
-                line_for_db[title] = data_array[i][j]
-            line_for_db["created_at"] = datetime.now()
-            line_for_db["updated_at"] = datetime.now()
-            data_for_db.append(line_for_db)
+            data_for_db = []
+            for i in range(len(data_array)):
+                line_for_db = {}
+                for j in range(len(title_array)):
+                    title = str(title_array[j])
+                    line_for_db[title] = data_array[i][j]
+                line_for_db["created_at"] = datetime.now()
+                line_for_db["updated_at"] = datetime.now()
+                data_for_db.append(line_for_db)
 
-        database = request.app.state.mongodb["Dina_Cargo"]
-        business_colection = database.get_collection("Data_Business_Colection")
+            database = request.app.state.mongodb["Dina_Cargo"]
+            business_colection = database.get_collection("data")
 
-        result = await business_colection.insert_many(data_for_db)
+            result = await business_colection.insert_many(data_for_db)
 
-        # * Временное решение для отладки
-        # with open("log.txt", "w", encoding="utf-8") as f:
-        #     for line in array_data:
-        #         f.writelines(str(line).replace("\n", "") + "\n")
+        else:
+            # limit the reading area
+            array_data = array_data[3:]
+            for i in range(len(array_data)):
+                array_data[i] = array_data[i][:20]
+
+            title_array = ['_id', 'Дата', 'Место отправки', 'Вид отправки', 'Телефон клиента', 'Код клиента', 'Описание груза ', 'Количество мест', 'Вес', 'Плотность места', 'Плотность',
+                           'Место доставки', 'За упаковку', 'За доставку', 'Разное', 'Страховка', 'Цена за единицу', 'Общая сумма', 'Дата прихода', 'Количество полученных позицый']
+            data_array = array_data[1:]
+
+            data_for_db = []
+            for i in range(len(data_array)):
+                line_for_db = {}
+                for j in range(len(title_array)):
+                    title = str(title_array[j])
+                    line_for_db[title] = data_array[i][j]
+                data_for_db.append(line_for_db)
+
+            database = request.app.state.mongodb["Dina_Cargo"]
+            business_colection = database.get_collection(
+                "data")
+
+            for line in data_for_db:
+                filter = {"_id": ObjectId(line['_id'])}
+
+                buf_data = line
+                del buf_data['_id']
+                buf_data["updated_at"] = datetime.now()
+                update = {"$set": buf_data}
+
+                result = await business_colection.find_one_and_update(filter, update)
+                if not result:
+                    buf_data["created_at"] = datetime.now()
+                    buf_data["updated_at"] = datetime.now()
+                    result = await business_colection.insert_one(buf_data)
+                    print(result.inserted_id)
+
+                # result = await business_colection.find_one(filter)
+                # result['_id'] = str(result['_id'])
+                # print('result: ', result)
 
         # Success
         return JSONResponse(content={"message": "File uploaded successfully"})
@@ -66,25 +143,30 @@ async def upload_file(request: Request, file: UploadFile = File(...)):
 async def export_excel(request: Request):
     try:
         database = request.app.state.mongodb["Dina_Cargo"]
-        business_colection = database.get_collection("Data_Business_Colection")
+        business_colection = database.get_collection("data")
 
         result = await business_colection.find({}).to_list(None)
 
         for i in range(0, len(result)):
-            del result[i]["_id"]
+            # del result[i]["_id"]
             del result[i]["created_at"]
             del result[i]["updated_at"]
             result[i]["_"] = ""
-            # for res in result[i].keys():
-            #     result[i][res] = str(result[i][res])
 
         # Create a DataFrame from the data
         df = pd.DataFrame(result)
+        df2 = pd.DataFrame({"created_at": [datetime.utcnow()]})
+
         # Create an Excel writer using BytesIO
         excel_writer = BytesIO()
-        # Write the DataFrame to the Excel file
-        df.to_excel(excel_writer, index=False, startrow=3,
-                    header="Dina_Cargo", engine="openpyxl")
+
+        # Write the DataFrames to separate sheets in the Excel file
+        with pd.ExcelWriter(excel_writer, engine="openpyxl") as writer:
+            df.to_excel(writer, sheet_name="data",
+                        index=False, startrow=4, header="data")
+            df2.to_excel(writer, sheet_name="__control_data",
+                         index=False, startrow=4, header="__control_data")
+
         # Set the file's name
         file_name = "exported_data.xlsx"
         # Prepare the response headers
@@ -109,7 +191,7 @@ async def export_excel(request: Request):
 async def upload_file(request: Request):
     try:
         database = request.app.state.mongodb["Dina_Cargo"]
-        business_colection = database.get_collection("Data_Business_Colection")
+        business_colection = database.get_collection("data")
 
         filter = {"_id": ObjectId("64bfaaf84582a086bb484b11")}
         update = {"$set": {"Вес": 100}}
@@ -128,7 +210,7 @@ async def upload_file(request: Request):
 async def upload_file(request: Request):
     try:
         database = request.app.state.mongodb["Dina_Cargo"]
-        business_colection = database.get_collection("Data_Business_Colection")
+        business_colection = database.get_collection("data")
 
         filter = {"_id": ObjectId("64bfaaf84582a086bb484b11")}
 
