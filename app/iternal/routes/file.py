5dd50c3cb1c2757c05d865d0a53f38074572708d
artem.drozdov <a.drozdov@gosu.kz>
2023-07-31 15:14:34 +0600

    rework upload

--- app/iternal/routes/file.py
+++ app/iternal/routes/file.py
@@ -13,58 +13,12 @@ router = APIRouter(
     tags=["Files"]
 )
 
-async def test(line, now, business_colection):
-
-    filter = {"_id": ObjectId(line['_id'])}
-
-    buf_data = line
-    del buf_data['_id']
-    buf_data["updated_at"] = now
-    update = {"$set": buf_data}
-
-    result = await business_colection.find_one_and_update(filter, update)
-    if not result:
-        buf_data["created_at"] = now
-        buf_data["updated_at"] = now
-        result = await business_colection.insert_one(buf_data)
-        print("create", result.inserted_id)
-    else:
-        # Calculate the number of modified fields
-        num_modified_fields = (
-            sum(1 for key, value in update["$set"].items() if result.get(key) != value) - 1)
-        if num_modified_fields > 0:
-            print("update", result.get('_id'), "count", num_modified_fields)
-    
-
-async def verification_upload_time(database, control_data, company_key: str) -> bool:
-    control_data_colection = database.get_collection("control_data")
-
-    upload_at = datetime.now().timestamp()
-    export_at = control_data.get("export_at")
-
-    filter = {"company_key": company_key}
-    result = await control_data_colection.find_one(filter)
-    if not result:
-        if export_at:
-            control_data_colection.insert_one(
-                {"company_key": company_key, "upload_at": export_at})
-            upload_at = export_at
-        else:
-            control_data_colection.insert_one(
-                {"company_key": company_key, "upload_at": upload_at})
-    else:
-        upload_at = result.get("upload_at")
-
-    if export_at and upload_at <= export_at:
-        update = {"$set": {"upload_at": export_at}}
-        result = await control_data_colection.find_one_and_update(filter, update)
-
-        return True
-    return False
-
 
 @router.post("/upload/")
 async def upload_file(request: Request, file: UploadFile = File(...)):
+    # Get now data
+    now = datetime.utcnow()
+
     # Read the Excel file using Pandas from the file's content
     content = await file.read()
 
@@ -72,96 +26,185 @@ async def upload_file(request: Request, file: UploadFile = File(...)):
 
     try:
         # Assuming the file is in .xlsx format.
-        control_data_df = pd.read_excel(
+        control_df = pd.read_excel(
             BytesIO(content), engine="openpyxl", sheet_name="__control_data")
 
         # Fill the empty with zero values ( df.fillna(0) )
-        control_data_df_filled_with_zero = control_data_df.where(
-            pd.notnull(control_data_df), None)
+        control_df_filled = control_df.where(pd.notnull(control_df), None)
 
-        # Convert to array
-        control_data_array_data = control_data_df_filled_with_zero.to_numpy().tolist()
+        # Convert to list
+        control_data_list = control_df_filled.to_numpy().tolist()
 
-        # Check if the array
-        if len(control_data_array_data) > 0:
+        # Check if the list
+        if len(control_data_list) > 0:
 
             # limit the reading area
-            control_data_array_keys = control_data_array_data[3:4]
-            control_data_array_data = control_data_array_data[4:5]
+            control_data_list_keys = control_data_list[3:4]
+            control_data_list = control_data_list[4:5]
 
             # Collect data from control_data sheet
-            for i in range(0, len(control_data_array_keys)):
-                key = control_data_array_keys[i][0]
-                data = control_data_array_data[i][0]
+            for i in range(0, len(control_data_list_keys)):
+                key = control_data_list_keys[i][0]
+                data = control_data_list[i][0]
                 control_data[key] = data
 
     except Exception as e:
         print({"message": str(e)})
-
-    now = datetime.now()
 
     try:
         # Assuming the file is in .xlsx format.
         df = pd.read_excel(BytesIO(content), engine="openpyxl")
 
         # Fill the empty with zero values ( df.fillna(0) )
-        df_filled_with_zero = df.where(pd.notnull(df), None)
-        array_data = df_filled_with_zero.to_numpy().tolist()  # Convert to array
+        df_filled = df.where(pd.notnull(df), None)
+        list_data = df_filled.to_numpy().tolist()  # Convert to list
 
-        if not len(control_data) > 0:
+        # Connect to DB connection
+        database = request.app.state.mongodb["Dina_Cargo"]
+        upload_colection = database.get_collection("upload")
+        data_colection = database.get_collection("data")
+
+        # Check if the control_data exists
+        if (len(control_data) > 0):
+
             # limit the reading area
-            # * -1 because we don't read the total and 4 because we don't read header
-            array_data = array_data[3:-1]
-            for i in range(len(array_data)):
-                array_data[i] = array_data[i][:19]
+            list_data = list_data[4:]
+            for i in range(len(list_data)):
+                list_data[i] = list_data[i][:20]
 
-            title_array = ['Дата', 'Место отправки', 'Вид отправки', 'Телефон клиента', 'Код клиента', 'Описание груза ', 'Количество мест', 'Вес', 'Плотность места', 'Плотность',
-                           'Место доставки', 'За упаковку', 'За доставку', 'Разное', 'Страховка', 'Цена за единицу', 'Общая сумма', 'Дата прихода', 'Количество полученных позицый']
-            data_array = array_data[1:]
+            title_list = ['_id', 'Дата', 'Место отправки', 'Вид отправки', 'Телефон клиента', 'Код клиента', 'Описание груза ', 'Количество мест', 'Вес', 'Плотность места', 'Плотность',
+                          'Место доставки', 'За упаковку', 'За доставку', 'Разное', 'Страховка', 'Цена за единицу', 'Общая сумма', 'Дата прихода', 'Количество полученных позицый']
 
-            data_for_db = []
-            for i in range(len(data_array)):
+            data_list = []
+            for i in range(len(list_data)):
                 line_for_db = {}
-                for j in range(len(title_array)):
-                    title = str(title_array[j])
-                    line_for_db[title] = data_array[i][j]
-                line_for_db["created_at"] = now
-                line_for_db["updated_at"] = now
-                data_for_db.append(line_for_db)
+                for j in range(len(title_list)):
+                    title = str(title_list[j])
+                    line_for_db[title] = list_data[i][j]
+                data_list.append(line_for_db)
 
-            database = request.app.state.mongodb["Dina_Cargo"]
-            business_colection = database.get_collection("data")
+            data_buf_list = []
 
-            result = await business_colection.insert_many(data_for_db)
+            for line in data_list:
+                buf_data = dict(line)
+                # Check if id field is exist and valid
+                if (ObjectId.is_valid(line['_id'])):
+                    filter = {"_id": ObjectId(line['_id'])}
+                    result = await data_colection.find_one(filter)
+                    # Check if product is exist
+                    if (result is not None):
+                        # Calculate the number of modified fields and check it
+                        sum_chenges = sum(1 for key, value in buf_data.items() if result.get(key) != value) - 1
+
+                        if sum_chenges > 0:
+                            buf_data["updated_at"] = now
+                            data_buf_list.append(buf_data)
+                    else:
+                        # If product not in connection
+                        del buf_data["_id"]
+                        buf_data["created_at"] = now
+                        buf_data["updated_at"] = now
+                        data_buf_list.append(buf_data)
+                else:
+                    # If add new product
+                    del buf_data["_id"]
+                    buf_data["created_at"] = now
+                    buf_data["updated_at"] = now
+                    data_buf_list.append(buf_data)
+
+            # Check if changes exist
+            if (len(data_buf_list) > 0):
+                data_for_db = dict()
+                data_for_db["created_at"] = now
+                data_for_db["control"] = control_data
+                data_for_db["data"] = data_buf_list
+
+                # * Есть вопросы, но пойдёт
+                result = await upload_colection.insert_one(data_for_db)
+
+                # Success
+                return JSONResponse(content={"message": "File pre-upload successfully", "data": str(result.inserted_id)})
+
         else:
             # limit the reading area
-            array_data = array_data[3:]
-            for i in range(len(array_data)):
-                array_data[i] = array_data[i][:20]
+            # * -1 because we don't read the total and 4 because we don't read header
+            list_data = list_data[3:-1]
+            for i in range(len(list_data)):
+                list_data[i] = list_data[i][:19]
 
-            title_array = ['_id', 'Дата', 'Место отправки', 'Вид отправки', 'Телефон клиента', 'Код клиента', 'Описание груза ', 'Количество мест', 'Вес', 'Плотность места', 'Плотность',
-                           'Место доставки', 'За упаковку', 'За доставку', 'Разное', 'Страховка', 'Цена за единицу', 'Общая сумма', 'Дата прихода', 'Количество полученных позицый']
-            data_array = array_data[1:]
+            title_list = ['Дата', 'Место отправки', 'Вид отправки', 'Телефон клиента', 'Код клиента', 'Описание груза ', 'Количество мест', 'Вес', 'Плотность места', 'Плотность',
+                          'Место доставки', 'За упаковку', 'За доставку', 'Разное', 'Страховка', 'Цена за единицу', 'Общая сумма', 'Дата прихода', 'Количество полученных позицый']
 
-            data_for_db = []
-            for i in range(len(data_array)):
+            data_list = []
+            for i in range(len(list_data[1:])):
                 line_for_db = {}
-                for j in range(len(title_array)):
-                    title = str(title_array[j])
-                    line_for_db[title] = data_array[i][j]
-                data_for_db.append(line_for_db)
+                for j in range(len(title_list)):
+                    title = str(title_list[j])
+                    line_for_db[title] = list_data[i][j]
+                line_for_db["created_at"] = now
+                line_for_db["updated_at"] = now
+                data_list.append(line_for_db)
 
-            database = request.app.state.mongodb["Dina_Cargo"]
-            business_colection = database.get_collection("data")
+            if (len(data_list) > 0):
+                data_for_db = dict()
+                data_for_db["created_at"] = now
+                data_for_db["data"] = data_list
 
-            if await verification_upload_time(request.app.state.database, control_data, "Dina_Cargo"):
-                for line in data_for_db:
-                    asyncio.create_task(test(line, now, business_colection))
-            else:
-                print("Error")
+                # * Есть вопросы, но пойдёт
+                result = await upload_colection.insert_one(data_for_db)
+
+                # Success
+                return JSONResponse(content={"message": "File pre-upload successfully", "data": str(result.inserted_id)})
 
         # Success
-        return JSONResponse(content={"message": "File uploaded successfully"})
+        return JSONResponse(content={"message": "File pre-upload successfully", "data": 0})
+    except Exception as e:
+        # Exception
+        return JSONResponse(content={"message": str(e)}, status_code=500)
+
+
+@router.post("/confirm/{id}")
+async def confirm_file(request: Request, id: str):
+    try:
+        database = request.app.state.mongodb["Dina_Cargo"]
+        upload_colection = database.get_collection("upload")
+
+        filter = {'_id': ObjectId(id)}
+
+        result = await upload_colection.find_one(filter)
+
+        # Check if id exists
+        if (result is not None):
+            data_for_db = result.get('data')
+            control_data = result.get('control')
+
+            data_colection = database.get_collection("data")
+
+            if (len(control_data) > 0):
+                insert_data = []
+                for line in data_for_db:
+
+                    data = dict(line)
+                    del data['_id']
+
+                    if (line.get('_id') is not None):
+                        filter = {'_id': ObjectId(line['_id'])}
+                        update = {"$set": data}
+                        result = await data_colection.find_one_and_update(filter, update)
+                        if (result is None):
+                            insert_data.append(data)
+                    else:
+                        insert_data.append(data)
+                if (len(insert_data) > 0):
+                    result = await data_colection.insert_many(insert_data)
+            else:
+                result = await data_colection.insert_many(data_for_db)
+
+        filter = {'_id': ObjectId(id)}
+        result = await upload_colection.delete_one(filter)
+
+        # Success
+        return JSONResponse(content={"message": "File confirm successfully"})
     except Exception as e:
         # Exception
         return JSONResponse(content={"message": str(e)}, status_code=500)
@@ -169,21 +212,23 @@ async def upload_file(request: Request, file: UploadFile = File(...)):
 
 @router.get("/export_excel/")
 async def export_excel(request: Request):
+    # Get now data
+    now = datetime.utcnow()
+
     try:
         database = request.app.state.mongodb["Dina_Cargo"]
-        business_colection = database.get_collection("data")
+        upload_colection = database.get_collection("data")
 
-        result = await business_colection.find({}).to_list(None)
+        result = await upload_colection.find({}).to_list(None)
 
         for i in range(0, len(result)):
-            # del result[i]["_id"]
             del result[i]["created_at"]
             del result[i]["updated_at"]
             result[i]["_"] = ""
 
         # Create a DataFrame from the data
         df = pd.DataFrame(result)
-        df2 = pd.DataFrame({"export_at": [datetime.utcnow().timestamp()]})
+        df2 = pd.DataFrame({"export_at": [now.timestamp()]})
 
         # Create an Excel writer using BytesIO
         excel_writer = BytesIO()
@@ -210,6 +255,5 @@ async def export_excel(request: Request):
         # Return the Excel file as a response
         return Response(content=excel_content, headers=response_headers)
     except Exception as e:
-        print('e: ', e)
         # Exception
-        return JSONResponse(content={"message": f"Не найден, обновление не выполнено."}, status_code=500)
+        return JSONResponse(content={"message": str(e)}, status_code=500)
